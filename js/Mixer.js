9ed02c1f525af029e1a0b6674ac442daeec6de0a
Cristhian Vega Cortes <cristhianv.info@gmail.com>
2016-07-18 16:31:08 -0400

    Trabajando en los sonidos

---
+++ js/Mixer.js
@@ -0,0 +1,30 @@
+function Mixer()
+{
+    this.soundFile = [];
+    this.sfx = [];
+    this.ctx;   
+}
+
+Mixer.prototype.init = function ()
+{
+    var audioContext = window.AudioContext || window.webkitAudioContext;
+    this.ctx = new audioContext();
+
+}
+
+Mixer.prototype.loadFile = function (file)
+{
+    return new Audio(file);
+}
+
+Mixer.prototype.loadSoundFile(id, file)
+{
+    this.soundFile[id] = this.loadFile(file);
+}
+
+Mixer.prototype.loadSfx(id, file)
+{
+    this.sfx[id] = this.loadFile(file);
+}
+
+Mixer.prototype.
